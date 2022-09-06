d991a2b37862b3d549d74055cda52be3510d1490
Sairon <sairon@example.com>
2022-09-06 22:25:57 +0300

    using Telegram Bot API

--- app/Facades/TgBot.php
+++ app/Facades/TgBot.php
@@ -4,7 +4,9 @@
 namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 
-
+/**
+ *  @method static void sendMessage(string $message)
+ */
 class TgBot extends Facade
 {
     protected static function getFacadeAccessor()
--- app/Services/TelegramBot.php
+++ app/Services/TelegramBot.php
@@ -3,7 +3,26 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class TelegramBot
 {
+    const url = 'https://api.tlgr.org/bot';
+    protected $bot;
+    protected $admin;
 
+
+    public function __construct($bot, $admin)
+    {
+        $this->bot = $bot;
+        $this->admin = $admin;
+    }
+
+    public function sendMessage($message, $user_id = null)
+    {
+        Http::post(self::url . $this->bot . '/sendMessage', [
+            'chat_id' => $user_id ?? $this->admin,
+            'text'=> $message
+        ]);
+    }
 }
